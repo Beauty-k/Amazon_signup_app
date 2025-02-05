88ef6322044e7836a052c3395ca10527d91c0500
Beauty kumari <sanjana98520@gmail.com>
2025-02-05 10:51:18 +0530

    Implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: session_params[:email])
     if user && user.authenticate(session_params[:password])
-    #  log_in user
+     log_in user
       
       flash[:success] = "you are successfully logged in"
       redirect_to user
@@ -17,7 +17,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil 
+    log_Out
+    # session[:user_id] = nil 
     flash[:success] = "You have logged out successfully"
     redirect_to root_path
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
-module ApplicationHelper < ActionController::Base
-    helper_method :current_cart
+module ApplicationHelper
+    # helper_method :current_cart
 
-    def current_cart
-        session[:cart_id] ||= Cart.create.id
-        cart.find(session[:cart_id])
-    end
+    # def current_cart
+    #     session[:cart_id] ||= Cart.create.id
+    #     cart.find(session[:cart_id])
+    # end
 
 end
